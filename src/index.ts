8201e813f4662f110f1a0673b666c8b10a9e98a3
张润钊 <zhangrunzhao@bytedance.com>
2023-03-26 22:25:37 +0800

    feat: 完成插件开发

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
-const printHelloWord = () => {
-  // eslint-disable-next-line no-console
-  console.log('hello world2');
+import { Plugin } from 'rollup';
+import WebSocket, { WebSocketServer } from 'ws';
+
+/**
+ * This is a hot update plugin for the development of chrome extensions
+ */
+const chromeExtensionsHmr = (): Plugin => {
+  let ws: WebSocket | null;
+  let wss: WebSocketServer | null;
+  let time: NodeJS.Timeout | null;
+
+  return {
+    name: 'chrome-extensions-hmr',
+
+    buildStart () {
+      // Create a ws server if you are in listening mode
+      if (this.meta.watchMode && !wss) {
+        wss = new WebSocketServer({ port: 7788 });
+        wss.on('connection', client => {
+          ws = client;
+        });
+      }
+    },
+
+    watchChange () {
+      time && clearTimeout(time);
+    },
+
+    writeBundle () {
+      if (this.meta.watchMode) {
+        time = setTimeout(() => {
+          ws?.send('reload-extension');
+        }, 500);
+      }
+    },
+
+    closeWatcher () {
+      ws?.close();
+      time && clearTimeout(time);
+    },
+  };
 };
-export { printHelloWord };
+
+export default chromeExtensionsHmr;
