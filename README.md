2a42ca2be06d2470692741e9dd5ec45ce125b0e8
张润钊 <zhangrunzhao@bytedance.com>
2023-03-26 22:50:57 +0800

    feat: 完成开源的基础配置

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-rollup 开发开源项目的轻量级环境
+基于 rollup/vite 开发浏览器插件的热更新插件
+
+# 1.0.0
+你必须在使用时在 background.js 中添加以下代码
+
+```
+const ws = new WebSocket('ws://localhost:7788');
+  ws.onmessage = (event) => {
+  if (event.data === 'reload-extension') {
+  chrome.runtime.reload();
+  }
+}
+```
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,4 @@
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
 import { RollupOptions } from 'rollup';
 
 const configs: RollupOptions[] = [
@@ -8,19 +6,10 @@ const configs: RollupOptions[] = [
     input: './src/index.ts',
     output: {
       dir: 'dist',
-      format: 'cjs',
-      entryFileNames: '[name].cjs.js',
-    },
-    plugins: [resolve(), commonjs(), typescript()],
-  },
-  {
-    input: './src/index.ts',
-    output: {
-      dir: 'dist',
       format: 'esm',
       entryFileNames: '[name].esm.js',
     },
-    plugins: [resolve(), commonjs(), typescript()],
+    plugins: [typescript()],
   },
 ];
 
